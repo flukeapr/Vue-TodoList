4505fbda6dbd2708408dc26d7ae97cb28b2223e8
flukeapr <wngsphlc@gmail.com>
2024-05-26 00:15:54 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import TodoList from './components/TodoList.vue';
+
+
+</script>
+
+<template>
+  
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
