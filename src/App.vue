defbca76b0fbd38fd1279944362cf9c465e15c47
flukeapr <wngsphlc@gmail.com>
2024-05-26 01:18:42 +0700

    add SpeedInsights

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 import TodoList from './components/TodoList.vue';
-
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 </script>
 
 <template>
-  
+  <SpeedInsights/>
   <RouterView/>
 </template>
 
